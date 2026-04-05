0c986fec892c63e77997a12e38882815d2b4f20b
Hadi F <62212402+hadi-f90@users.noreply.github.com>
2026-04-05 13:06:21 +0330

    Styling and removing an error for submision

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -24,5 +25,7 @@ urlpatterns = [
     path('', include('directory.urls')),
 ]
 
+# Serve static and media files in development
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- directory/forms.py
+++ directory/forms.py
@@ -1,8 +1,44 @@
 from django import forms
-
+from .models import Website, Category
 
 class WebsiteSubmitForm(forms.ModelForm):
-    pass
+    """Form for users to submit new websites"""
+    class Meta:
+        model = Website
+        fields = ['title', 'url', 'description', 'category', 'owner_name', 'owner_email']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Website Title'
+            }),
+            'url': forms.URLInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'https://example.com'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Describe your website...'
+            }),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'owner_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Your Name'
+            }),
+            'owner_email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'your@email.com'
+            }),
+        }
+
 
 class WebsiteAdminForm(forms.ModelForm):
     """Form for admin to edit website with all fields"""
+    class Meta:
+        model = Website
+        fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'vTextField'}),
+            'url': forms.URLInput(attrs={'class': 'vTextField'}),
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
